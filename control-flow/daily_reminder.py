a7e6512cf041707a7152fde3ee6243137900a221
Dreamboat <enechimdindu@gmail.com>
2024-09-30 10:06:10 +0100

    reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,24 +4,24 @@ time_bound = input('Is it time-bound? (yes/no): ').lower
 match priority:
     case _ if priority == 'high':
         if time_bound == 'yes':
-            print(f"'{task}' is a {time_bound} priority task that requires immediate attention today!")
+            print(f"Reminder: '{task}' is a {time_bound} priority task that requires immediate attention today!")
         elif time_bound == 'no':
-            print(f"'{task}' is a {time_bound} priority task. Consider completing it when you have free time.")
+            print(f"Note: '{task}' is a {time_bound} priority task. Consider completing it when you have free time.")
         else:
             print('error')
     case _ if priority == 'medium':
         if time_bound == 'yes':
-            print(f"'{task}' is a {time_bound} priority task that requires immediate attention today!")
+            print(f"Reminder: '{task}' is a {time_bound} priority task that requires immediate attention today!")
         elif time_bound == 'no':
-            print(f"'{task}' is a {time_bound} priority task. Consider completing it when you have free time.")
+            print(f"Note: '{task}' is a {time_bound} priority task. Consider completing it when you have free time.")
         else:
             print('error')
     case _ if priority == 'low':
         if time_bound == 'yes':
-            print(f"'{task}' is a {time_bound} priority task that requires immediate attention today!")
+            print(f"Reminder: '{task}' is a {time_bound} priority task that requires immediate attention today!")
         elif time_bound == 'no':
-            print(f"'{task}' is a {time_bound} priority task. Consider completing it when you have free time.")
+            print(f"Note: '{task}' is a {time_bound} priority task. Consider completing it when you have free time.")
         else:
             print('error')
-    case _ :
+    case _:
         print('error')
