e6032c14bf01d16155f7edfd43ae5c7ce8b0df9a
Dreamboat <enechimdindu@gmail.com>
2024-10-09 00:06:28 +0100

    performing basic operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,6 +3,7 @@ def perform_operation (num1, num2, operation):
     num1 = int(input('Enter the first number: '))
     num2 = int(input('Enter the second number: '))
     operation = input('Enter the operation (add, subtract, multiply, divide): ').lower()
+    result = 0
     
     if operation == 'add':
         result = num1 + num2
