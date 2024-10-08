2c595d1886e1885bbc9c5263118957392c849971
Dreamboat <enechimdindu@gmail.com>
2024-10-09 00:23:12 +0100

    performing basic operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,7 +1,7 @@
-def perform_operation (num1, num2, operation):
+def perform_operation(num1, num2, operation):
     
-    num1 = int(input('Enter the first number: '))
-    num2 = int(input('Enter the second number: '))
+    num1 = float(input('Enter the first number: '))
+    num2 = float(input('Enter the second number: '))
     operation = input('Enter the operation (add, subtract, multiply, divide): ').lower()
     result = 0
     
@@ -15,7 +15,10 @@ def perform_operation (num1, num2, operation):
         result = num1 * num2
     
     elif operation == 'divide':
-        result = num1 / num2
+        if num2 == 0:
+            print('error; division by zero is underfined')
+        else:
+            result = num1 / num2
     
     else:
         print('error')
