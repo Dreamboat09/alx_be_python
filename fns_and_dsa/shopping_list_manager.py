7533970f93f72e2adddf29722eef22a03b76da50
Dreamboat <enechimdindu@gmail.com>
2024-10-09 01:10:48 +0100

    shopping list manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,30 @@
+shopping_list = []
+
+while True:
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+    
+    choice = input("Enter your choice: ")
+
+    
+    if choice == '1':
+        item = input('add what item? ')
+        shopping_list.append(item)
+        
+    elif choice == '2':
+        item = input('remove what item? ')
+        shopping_list.remove(item)
+    
+    elif choice == '3':
+        for things in shopping_list:
+            print(things)
+    
+    elif choice == '4':
+        print("Goodbye!")
+        break
+    
+    else:
+        print("Invalid choice. Please try again.") 
