dd4e297c54975d83d99ca398ac66b07d0d24e9c4
Dreamboat <enechimdindu@gmail.com>
2024-10-11 18:52:06 +0100

    temp convertion

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,28 +1,30 @@
-temp = int(input('Enter the temperature to convert: '))
-unit = input('Is this temperature in Celsius or Fahrenheit? (C/F): ').lower()
-
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+temp = float(input('Enter the temperature to convert: '))
+unit = input('Is this temperature in Celsius or Fahrenheit? (C/F): ').lower()
+
 def convert_to_celsius(fahrenheit):
     global FAHRENHEIT_TO_CELSIUS_FACTOR
-    celsius = FAHRENHEIT_TO_CELSIUS_FACTOR
-    print(f'{temp} is {celsius}')
-    
-    convert_to_celsius(fahrenheit)
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+   
+    return celsius
+
     
 def convert_to_fahrenheit(celsius):
     global CELSIUS_TO_FAHRENHEIT_FACTOR
-    fahrenheit = CELSIUS_TO_FAHRENHEIT_FACTOR
-    print(f'{temp} is {fahrenheit}')
+    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     
-    convert_to_fahrenheit(celsius)
+    return fahrenheit
     
     
-if unit == 'f':
-    convert_to_fahrenheit()
+if unit == 'c':
+    fahrenheit = convert_to_fahrenheit(temp)
+    print(f'{temp}°C is {fahrenheit}°F')
 
-elif unit == 'c':
-    convert_to_fahrenheit
-     
+elif unit == 'f':
+    celsius = convert_to_celsius(temp)
+    print(f'{temp}°F is {celsius}°C')
+    
 else:
     print('Invalid temperature. Please enter a numeric value.')
