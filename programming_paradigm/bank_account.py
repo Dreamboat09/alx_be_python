e67fc3b12f11907379fe5bd8ff0a4c841bbc16bf
Dreamboat <enechimdindu@gmail.com>
2024-10-19 23:06:54 +0100

    bank account OOP

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,17 +4,14 @@ class BankAccount:
         account_balance = 0
     def deposit(self, amount):
         self.account_balance += amount
-        print(f'Deposited: ${self.account_balance}')
     
     def withdraw(self, amount):
         if self.account_balance < amount:
-            print('Insufficient funds.')
             return False
     
         else:
             self.account_balance -= amount
-            print(f'withdraw:${self.account_balance}')
         
     def display_balance(self):
-        print(f'Current Balance: ${self.account_balance}')
+        return {self.account_balance}
         
