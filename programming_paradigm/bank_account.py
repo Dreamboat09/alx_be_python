24fdefb49bbd21c409080ba6d234e2eac595ada7
Dreamboat <enechimdindu@gmail.com>
2024-10-19 23:25:16 +0100

    bank account OOP

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,12 +6,12 @@ class BankAccount:
         self.account_balance += amount
     
     def withdraw(self, amount):
-        if self.account_balance < amount:
-            return False
-    
-        else:
+        if self.account_balance > amount:
             self.account_balance -= amount
+        else:
+            return False
+            
         
     def display_balance(self):
-        print(f'Current Balance: ${self.account_balance}')
+        print(f'Current Balance: ${self.account_balance}.00')
         
