a9f98c70a06c23a133739c4e4d27ac6331a41c8e
Dreamboat <enechimdindu@gmail.com>
2024-10-23 21:45:17 +0100

    library management system

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,27 +1,39 @@
 class Book:
-    def __init__(self, title, author, _is_checked_out):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
-        self._is_checked_out = _is_checked_out
+        self._is_checked_out = True 
         
     def check_out(self):
-        pass
+        if self.title and self.author:
+            return self._is_checked_out()
     
     def return_book(self):
-        pass
+        if self.title and self.author:
+            return not self._is_checked_out()
     
-class Library:
+class Library(Book):
     def __init__(self):
         self._books = []
+        self._available = True
         
     def add_book(self):
-        pass
+        self._books = self._books.append(Book)
     
     def check_out_book(self, title):
-        pass
+        for BOOK in self._books:
+            if BOOK.title == title and self._available:
+                print(f'{title} is ckecked out')
+                self._books.remove(title)
+        
     
     def return_book(self, title):
-        pass
+        for BOOK in self._books:
+            if BOOK.title == title and not self._available:
+                print(f'{title} returned')
+                self._books.append(title)
+        
     
     def list_available_books(self):
-        pass 
+       for BOOK in self._books and self._available:
+           print(BOOK)
