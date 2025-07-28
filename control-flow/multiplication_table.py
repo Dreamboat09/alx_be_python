92655cd1cf4b1108e5fbafb0764fef9a4f92f15b
Dreamboat09 <enechimdindu@gmail.com>
2025-07-28 11:43:20 +0100

    utilizing 'FOR' statement to create multiplication table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,5 @@ number = int(input("Enter a number to see its multiplication table: "))
 
 # for conditional statement to iterate over the range of numbers from 1 to 10
 for i in range(1, 11):
-    print(f"{number} x {i} = {number * i}")  # f-string
+    print(f"{number} * {i} = {number * i}")  # f-string
+
