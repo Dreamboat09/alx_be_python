d49fb26da6527b56bc4079cc7b9cae8a059bb71a
Dreamboat09 <enechimdindu@gmail.com>
2025-08-07 10:08:58 +0100

    basic arithmetic operation function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,7 +6,7 @@ def perform_operation(num1, num2, operation):
         return num1 + num2
     
     elif operation == "subtract":
-        return num1 - num1
+        return num1 - num2
     
     elif operation == "multiply":
         return num1 * num2
