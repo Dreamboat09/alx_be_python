272bc9e6099e62f6d417a6d2a14f08939f935560
Dreamboat09 <enechimdindu@gmail.com>
2025-08-07 10:01:02 +0100

    basic arithmetic operation function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,22 @@
+# define the function and assign parameters to it
+
+def perform_operation (num1=float, num2=float, operation=str):
+    # perform the operations
+    if operation == "add":
+        return num1 + num2
+    
+    elif operation == "subtract":
+        return num1 - num1
+    
+    elif operation == "multiply":
+        return num1 * num2
+    
+    elif operation == "divide":
+        # handling division by zero
+        if num2 == 0:
+            return "indivisible by zero(0)"
+        else:
+            return num1 / num2
+    
+    else:
+        return 'wrong operation'
