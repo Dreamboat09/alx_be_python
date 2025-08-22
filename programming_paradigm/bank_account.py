88b11869347af91a2c8a593e79511fa25565fd00
Dreamboat09 <enechimdindu@gmail.com>
2025-08-22 22:07:56 +0100

    creating a bank account class to deposit, withdraw and display balance

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,21 @@
+# defining a class
+class BankAccount:
+
+    # initiator method
+    def __init__ (self, account_balance: int = 0):
+        self.account_balance = account_balance
+
+    # defining a method to deposit an amount
+    def deposit(self, amount: int): 
+        self.account_balance  += amount
+
+    # defining a method to withdraw an amount 
+    def withdraw(self, amount: int):
+        if self.account_balance > amount:
+            self.account_balance -= amount
+        else:
+            return False
+        
+    # defining a method to display balance
+    def  display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
