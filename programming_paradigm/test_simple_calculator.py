f473a88a13ed35c0cfb44c1cbf09d14cc20a6635
Dreamboat09 <enechimdindu@gmail.com>
2025-08-24 21:07:11 +0100

    exploring unittest on simple calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,34 +5,34 @@ from simple_calculator import SimpleCalculator
 class test_simple_calculator(unittest.TestCase):
 
     def setUp(self):
-        self.Calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
 
     def test_addition(self):
-        self.assertEqual(self.Calculator.add(2, 5), 7)
-        self.assertEqual(self.Calculator.add(-2, -5), -7)
-        self.assertEqual(self.Calculator.add(-1, 5), 4)
-        self.assertEqual(self.Calculator.add(1, -5), -4)
-        self.assertEqual(self.Calculator.add(10, 0), 10)
+        self.assertEqual(self.calc.add(2, 5), 7)
+        self.assertEqual(self.calc.add(-2, -5), -7)
+        self.assertEqual(self.calc.add(-1, 5), 4)
+        self.assertEqual(self.calc.add(1, -5), -4)
+        self.assertEqual(self.calc.add(10, 0), 10)
 
     def test_subtraction(self):
-        self.assertEqual(self.Calculator.subtract(10, 7), 3)
-        self.assertEqual(self.Calculator.subtract(-2, -5), 3)
-        self.assertEqual(self.Calculator.subtract(-1, 5), -6)
-        self.assertEqual(self.Calculator.subtract(10, 0), 10)
-        self.assertEqual(self.Calculator.subtract(1, -5), 6)
+        self.assertEqual(self.calc.subtract(10, 7), 3)
+        self.assertEqual(self.calc.subtract(-2, -5), 3)
+        self.assertEqual(self.calc.subtract(-1, 5), -6)
+        self.assertEqual(self.calc.subtract(10, 0), 10)
+        self.assertEqual(self.calc.subtract(1, -5), 6)
 
 
     def test_multiply(self):
-        self.assertEqual(self.Calculator.multiply(2, 5), 10)
-        self.assertEqual(self.Calculator.multiply(-2, -5), 10)
-        self.assertEqual(self.Calculator.multiply(-1, 5), -5)
-        self.assertEqual(self.Calculator.multiply(0, -5), 0)
+        self.assertEqual(self.calc.multiply(2, 5), 10)
+        self.assertEqual(self.calc.multiply(-2, -5), 10)
+        self.assertEqual(self.calc.multiply(-1, 5), -5)
+        self.assertEqual(self.calc.multiply(0, -5), 0)
 
     def test_divide(self):
-        self.assertEqual(self.Calculator.divide(5, 2), 2.5)
-        self.assertEqual(self.Calculator.divide(20, -5), -4)
-        self.assertEqual(self.Calculator.divide(-100, 5), -20)
-        self.assertEqual(self.Calculator.divide(10, 0), None)
+        self.assertEqual(self.calc.divide(5, 2), 2.5)
+        self.assertEqual(self.calc.divide(20, -5), -4)
+        self.assertEqual(self.calc.divide(-100, 5), -20)
+        self.assertEqual(self.calc.divide(10, 0), None)
 
 if __name__ == '__main__':
     unittest.main()
