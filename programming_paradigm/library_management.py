4c83c272afec3fbe49d5bec707e7162e7d2453b2
Dreamboat09 <enechimdindu@gmail.com>
2025-08-28 23:49:23 +0100

    understanding of basic OOP concepts by implementing a system that tracks books in a library

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,35 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self, title):
+        if title == self.title:
+            self._is_checked_out = False
+
+    def check_in(self, title):
+        if title == self.title:
+            self._is_checked_out = True
+
+
+
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, Book):
+        self._books.append(Book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if title == book.title:
+                return self._is_checked_out
+
+    def return_book(self, title):
+        for book in self._books:
+            return self._is_checked_out
+        
+    def list_available_books(self):
+        for book in self._books:
+            return book 
