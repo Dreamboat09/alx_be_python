4f2c1eb4e48903ff05d04797b5c36100840545b8
Dreamboat09 <enechimdindu@gmail.com>
2025-09-06 15:49:17 +0100

    exploring magic method

---
+++ oop/book_class.py
@@ -0,0 +1,19 @@
+#creation of Book class
+class Book:
+    #creation of the constructor
+    def __init__(self, title:str, author:str, year:int):
+        self.title = title
+        self.author = author
+        self.year = year
+    
+    #creation of the destructor
+    def __del__(self, title):
+        print(f"Deleting {title}")
+    
+    #creation of the String Representation 
+    def __str__(self):
+        return "{title} by {author}, published in {year}"
+
+    #creation of the Official Representation
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
